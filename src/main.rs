9589da1a0940039fe8887830326f3f864469af3f
Tristram Oaten <tristram@oaten.name>
2023-04-01 14:31:26 +0100

    new website!

--- src/main.rs
+++ src/main.rs
@@ -1,180 +1,111 @@
 use color_eyre::Report;
 use yate::html;
 
-
 fn template(inner: String) -> String {
     html! {
 
-        <!DOCTYPE html>
-        <html lang="en">
+           <!DOCTYPE html>
+           <html lang="en">
 
-        <head>
-            <meta http-equiv="x-clacks-overhead" content="GNU Terry Pratchett" />
-            <link rel="icon" href="favicon.png"/>
-            <link href="bulma-min.css" rel="stylesheet"/>
-            <script src="https://kit.fontawesome.com/333f3de551.js" crossorigin="anonymous"></script>
+           <head>
+               <meta http-equiv="x-clacks-overhead" content="GNU Terry Pratchett" />
+               <link rel="icon" href="favicon.png"/>
 
-            <meta charset="utf-8"/>
-            <meta name="description" content="/usr/stories podcast"/>
-            <meta content="width=device-width, initial-scale=1" name="viewport"/>
-            <script src="https://kit.fontawesome.com/333f3de551.js" crossorigin="anonymous"></script>
-            <title>"/usr/stories podcast"</title>
-            <style>"
-                .navbar-item, .navbar-link, .has-dropbown :hover {
-                    background-color: #0a0a0a !important;
-                }
-                .navbar-link:not(.is-arrowless)::after {
-                    border-color: white;
-                }
-                .navbar-burger {
+               <meta charset="utf-8"/>
+               <meta name="description" content="/usr/stories podcast"/>
+               <meta content="width=device-width, initial-scale=1" name="viewport"/>
+               <title>"/usr/stories podcast"</title>
+
+               <style>"
+                  body {
+                    font: 100% Courier New, monospace;
+                    margin: 20px;
+                    line-height: 26px;
+                    background-color: #000000;
+                    padding: 10px;
+                    color: #84c754;
+                  }
+                  a {
                     color: white;
-                }
-                //.hero {
-                //    background: #000 url(bg.jpg) center / cover;
-                //}
+                  }
+                  .glow {
+                    color: #84c754;
+                    -webkit-animation: glow 1s linear infinite alternate;
+                    -moz-animation: glow 1s linear infinite alternate;
+                    animation: glow 1s linear infinite alternate;
+                    text-shadow: 0 0 5px #fff;
+                  }
+                  @-webkit-keyframes glow {
+                    from {
+                      text-shadow: 0 0 10px #fff;
+                    }
+                    to {
+                      text-shadow: 0 0 15px #fff;
+                    }
+                  }
+                  .narrow {
+                     max-width: 50%;
+                  }
             "</style>
-        </head>
+           </head>
 
+           {inner}
 
-    <body class="is-family-monospace">
-        <section class="hero"/>
+     <script src="https://unpkg.com/typeit@8.7.1/dist/index.umd.js"></script>
+     <script language="javascript">
+     r##"
+      new TypeIt("#typed", {
+        speed: 1,
+        loop: false,
+        html: true,
+        nextStringDelay: 0
+      }).go();
+     "##
+     </script>
 
-            <nav class="navbar has-background-black" role="navigation" aria-label="main navigation">
-                <div class="navbar-brand">
-
-                    <a role="button" class="navbar-burger burger has-color-white" aria-label="menu" aria-expanded="false" data-target="navbar">
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                    </a>
-                </div>
-
-                <div id="navbar" class="navbar-menu has-background-black">
-                    <div class="navbar-start">
-
-/*
-                        <a class="navbar-item has-text-white" href="store.html">
-
-                            <span class="icon is-small">
-
-                          <i class="fas fa-box"></i>
-                        </span>
-                          "&nbsp;Store"
-                        </a>
-*/
-                        <a class="navbar-item has-text-white" href="https://twitter.com/usrstories">
-                            <span class="icon is-small">
-                          <i class="fab fa-twitter"></i>
-                        </span>
-                          "&nbsp;Follow on Twitter"
-                        </a>
-
-                        //<a class="navbar-item has-text-white" href="https://www.youtube.com/watch?v=Q3AhzHq8ogs&list=PLZaoyhMXgBzoM9bfb5pyUOT3zjnaDdSEP&index=1">
-                        //  <span class="icon is-small">
-                        //    <i class="fab fa-youtube"></i>
-                        //  </span>
-                        //  "&nbsp;Watch on YouTube"
-                        //  </a>
-
-                    <a class="navbar-item has-text-white" href="https://discord.gg/mCY2bBmDKZ">
-
-                      <span class="icon is-small">
-                        <i class="fab fa-discord"></i>
-                      </span>
-                      "&nbsp;Chat on Discord"
-                    </a>
-
-
-                    </div>
-                    
-                    //<div class="navbar-end">
-                    //    <div class="navbar-item">
-                    //        <div class="buttons">
-                    //            <a class="button" href="https://www.patreon.com/noboilerplate">
-                    //          <i class="fab fa-patreon"></i>
-                    //      "&nbsp;Support me on Patreon"
-                    //        </a>
-                    //        </div>
-                    //    </div>
-                    //</div>
-                </div>
-            </nav>
-            <section class="hero is-fullheight-with-navbar has-background-black has-text-light">
-                <div class="hero-body">
-                    <container class="container">
-                        <div class="columns">
-                            <div class="column is-three-fifths is-offset-one-fifth">
-
-                            {inner}
-
-
-                            </div>
-                        </div>
-                    </container>
-                </div>
-            </section>
-        <footer class="footer has-text-white has-background-black">
-        <div class="content has-text-centered">
-            <p>
-                    "We do not collect your data. Stop sending it to us."
-            </p>
-        </div>
-        </footer>
-    </body>
-
-        <script language="javascript">
-        r#"
-    document.addEventListener('DOMContentLoaded', () => {
-
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-
-    // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
-
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
-      });
-    });
-  }
-
-      });
-"#
-</script>
-        </html>
-         }
+     </html>
+    }
 }
-
 
 fn index() -> String {
-    template(html!{
-        //<figure class="image">
-        //    <img alt="logo" src="logo.png"/>
-        //</figure>
+    template(html! {
+    <body class="glow">
+      <center>
+        <a href="">"Listen"</a>"&nbsp;&nbsp;"
+        <a href="https://wandering.shop/@usrstories">"Follow us on Mastodon"</a>"&nbsp;&nbsp;"
+        //<a href="credits.html">"Credits"</a>"&nbsp;&nbsp;"
+        <a href="https://discord.gg/mCY2bBmDKZ">"Chat on Discord"</a>"&nbsp;&nbsp;"
+        <a href="">"Support us on Patreon"</a>"&nbsp;&nbsp;"
+        <br/>
+        <br/>
+        <h1><a href="">"/usr/stories"</a>" are hard scifi tales<br/><br/>from the computer revolution."</h1>
+        <div class="narrow">
+        </div>
+      </center>
+      <br/>
+      <br/>
+      "
+        $ cd season1/<br>
+        $ ls -al<br>
+      "
+      <div id="typed">r#"
+        total 10<br/>
+        -rw-r--r-- 1 oatman oatman    0 Jan  1 00:00 <a href="">episode0.mp3</a><br/>
+        -rw-r--r-- 1 oatman oatman    0 Jan  1 00:00 episode1.mp3<br/>
+        -rw-r--r-- 1 oatman oatman    0 Jan  1 00:00 episode2.mp3<br/>
+        -rw-r--r-- 1 oatman oatman    0 Jan  1 00:00 episode3.mp3<br/>
+        -rw-r--r-- 1 oatman oatman    0 Jan  1 00:00 episode4.mp3<br/>
+        -rw-r--r-- 1 oatman oatman    0 Jan  1 00:00 episode5.mp3<br/>
+        -rw-r--r-- 1 oatman oatman    0 Jan  1 00:00 episode6.mp3<br/>
+        -rw-r--r-- 1 oatman oatman    0 Jan  1 00:00 episode7.mp3<br/>
+        -rw-r--r-- 1 oatman oatman    0 Jan  1 00:00 episode8.mp3<br/>
+        -rw-r--r-- 1 oatman oatman    0 Jan  1 00:00 episode9.mp3<br/>
+        <a href="">$ play episode0.mp3</a><br/>
+      "#</div>
 
-        <span class="is-family-monospace is-size-4">
-            "/usr/stories are hard scifi tales<br/> from the computer revolution."
-
-        </span>
-
-        <p class="has-text-center">
-            <a href="https://twitter.com/usrstories" class="button has-text-right">
-                "START HERE"
-            </a>
-        </p>
-    })
+    </body>
+      })
 }
-
 
 fn main() -> Result<(), Report> {
     std::fs::create_dir_all("docs")?;
